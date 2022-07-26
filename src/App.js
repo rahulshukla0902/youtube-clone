0f5689fd776b96540a6235c5b6af75707caf1ddf
rahulshukla0902 <77751917+rahulshukla0902@users.noreply.github.com>
2022-07-27 00:51:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,29 +3,15 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import RecommendedVideos from './RecommendedVideos';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Router>
-        <Switch>
-          <Route path="/search">
-            <h1>Search Page</h1>
-          </Route>
-          <Route path="/">
-            <h1>Home Page</h1>
-          </Route>
-        </Switch>
-      </Router>
-
-
-
-      {/* <Header />
+      <Header />
       <div className="app__page">
         <Sidebar />
         <RecommendedVideos />
-      </div> */}
+      </div> 
     </div>
   );
 }
